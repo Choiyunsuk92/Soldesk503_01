59b2378450eb0ec9652a225e817e1516b84d3c87
soongmoorhee <soongmoorhee@localhost>
2020-09-08 14:12:34 +0900

    2020090803

--- testSpringBoot/src/main/java/testSpringBoot/controller/library/LibraryController.java
+++ testSpringBoot/src/main/java/testSpringBoot/controller/library/LibraryController.java
@@ -1,13 +1,49 @@
 package testSpringBoot.controller.library;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import testSpringBoot.command.LibraryBoardCommand;
+import testSpringBoot.service.libraryBoard.LibraryBoardService;
 
 @Controller
 @RequestMapping("libraryBoard")
 public class LibraryController {
+	@Autowired
+	LibraryBoardService libraryBoardService;
+	
+	// command객체가 필요한 곳에 model로 전달 
+	@ModelAttribute
+	LibraryBoardCommand setLibraryBoardCommand() {
+		return new LibraryBoardCommand();
+	}
+	// model.addAttribute("libraryBoardCommand", new LibraryBoardCommand())
+	// @ModelAttribute("libraryBoardCommand" )LibraryBoardCommand libraryBoardCommand
 	@RequestMapping("library")
 	public String libraryList() {
 		return "lib_Board/lib_board_list";
+	}
+	
+	@RequestMapping(value="libBoardForm" , method = RequestMethod.GET)
+	public String libraryWrite(/*LibraryBoardCommand libraryBoardCommand*/) {
+		return "thymeleaf/lib_Board/lib_board_write";
+	}
+	@RequestMapping(value="libBoardForm" , method = RequestMethod.POST)
+	public String libraryWritePro(@Validated LibraryBoardCommand libraryBoardCommand,
+			BindingResult result, HttpServletRequest request) {
+		if (result.hasErrors()) {
+			System.out.println("bsaicf");
+ 			 return "thymeleaf/lib_Board/lib_board_write";
+	    }
+		libraryBoardService.writePro(libraryBoardCommand,request);
+		//write페이지가 실행된 후에는 리스트 페이지로 가야 한다.
+		return "redirect:/libraryBoard/library";
 	}
 }
---
+++ testSpringBoot/src/main/java/testSpringBoot/service/libraryBoard/LibraryBoardService.java
@@ -0,0 +1,16 @@
+package testSpringBoot.service.libraryBoard;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import testSpringBoot.command.LibraryBoardCommand;
+
+@Component
+@Service
+public class LibraryBoardService {
+	public void writePro(LibraryBoardCommand libraryBoardCommand, HttpServletRequest request) {
+		
+	}
+}
