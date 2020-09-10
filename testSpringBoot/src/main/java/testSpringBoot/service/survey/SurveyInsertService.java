d7c571ec371e49271c6463562d7438b4fb2ad5b9
soongmoorhee <soongmoorhee@172.16.3.9>
2020-09-10 14:57:32 +0900

    2020091007

--- testSpringBoot/src/main/java/testSpringBoot/service/survey/SurveyInsertService.java
+++ testSpringBoot/src/main/java/testSpringBoot/service/survey/SurveyInsertService.java
@@ -19,20 +19,26 @@ public class SurveyInsertService {
 	public void surveyInsert(SurveyCommend surveyCommend, HttpSession session) 
 			throws Exception{
 		String userId = ((AuthInfo)session.getAttribute("authInfo")).getId();
+		// 문제가 하나일 
 		QuestionDTO qdto =  new QuestionDTO();
 		qdto.setQuestionTitle(surveyCommend.getQuestion());
 		qdto.setUserId(userId);
 		surveyMapper.questionInsert(qdto);
 		//question 저장되고 나서 questionNum를 받아오기 위해 쿼리문 실행 
 		int questionNum = surveyMapper.questionNum(userId);
-		String options [] = surveyCommend.getOptions().split("`");
-		int optionsNum = 1;
+		String options [] = null;
+		if(surveyCommend.getOptions() != null) {
+				surveyCommend.getOptions().split("`");
+		}
+		int optionsNum = 1; //option 번호생성 
+		// 문항은 1개 이상 
 		if(options != null) {
+			// n개만큼 문항이 저장이 되도록 반복구문 사용 
 			for(String optionName : options) {
 				OptionsDTO odto = new OptionsDTO();
 				odto.setQuestionNum(questionNum);
 				odto.setOptionName(optionName);
-				odto.setOptionNum(optionsNum++);
+				odto.setOptionNum(optionsNum++); // option번호를 option 만큼 1씩증
 				odto.setUserId(userId);
 				surveyMapper.optionInsert(odto);
 			}
