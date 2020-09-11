b4e2bfe20418160c28673b6072520a22a5b51950
soongmoorhee <soongmoorhee@localhost>
2020-09-11 13:19:28 +0900

    2020091102

---
+++ testSpringBoot/src/main/java/testSpringBoot/command/DateCommend.java
@@ -0,0 +1,10 @@
+package testSpringBoot.command;
+
+import lombok.Data;
+
+@Data
+public class DateCommend {
+	String nYear;
+	String nMonth;
+	String action;
+}
---
+++ testSpringBoot/src/main/java/testSpringBoot/controller/schedule/ScheduleController.java
@@ -0,0 +1,22 @@
+package testSpringBoot.controller.schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import testSpringBoot.command.DateCommend;
+import testSpringBoot.service.DateService.DateService;
+
+@Controller
+@RequestMapping("schedule")
+public class ScheduleController {
+	@Autowired
+	DateService dateService ;
+	@RequestMapping("schedule")
+	public String form(DateCommend dateCommend, Model model) {
+		dateService.execute(dateCommend, model);
+		return "thymeleaf/schedule/schedule";
+	}
+	
+}
